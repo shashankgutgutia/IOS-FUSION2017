5ddd94b0520cae1d0feb4bc819635902e335e8cc
Shashank Gutgutia <shashankgutgutia@gmail.com>
2017-09-02 19:14:49 +0530

    Revert "Added qr scanner in the main activity"

--- app/src/main/java/com/adgvit/iosfusion2017/MainActivity.java
+++ app/src/main/java/com/adgvit/iosfusion2017/MainActivity.java
@@ -1,41 +1,25 @@
 package com.adgvit.iosfusion2017;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private ImageView arrow;
     private GestureDetectorCompat gestureObject;
-    private IntentIntegrator qrScan;
-    private ImageView swipeButton;
-    private String name, reg_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        swipeButton = (ImageView) findViewById(R.id.arrow);
-        swipeButton.setOnClickListener(this);
-
         gestureObject = new GestureDetectorCompat(this, new LearnGesture());
-
-        qrScan = new IntentIntegrator(this);
-        
         arrow = (ImageView) findViewById(R.id.arrow);
         arrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,12 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onTouchEvent(event);
     }
 
-    @Override
-    public void onClick(View v) {
-        //check for authentication
-        qrScan.initiateScan();
-    }
-
     public class LearnGesture extends GestureDetector.SimpleOnGestureListener {
 
         //SimpleGestureListener is the listener for what type of swipe we want
@@ -68,41 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
             if(event2.getY() < event1.getY()) {
                 //down to up swipe
-                //check for authentication here
-                qrScan.initiateScan();
+                Intent in = new Intent(MainActivity.this, QrActivity.class);
+                startActivity(in);
             }
             else if(event2.getY() > event1.getY()) {
                 //up to down swipe
             }
             return true;
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                //if qrCode is empty
-                Toast.makeText(this, "Result not found", Toast.LENGTH_LONG).show();
-            } else {
-                //if qrCode has data
-                try {
-                    //converting the data to JSON
-                    JSONObject obj = new JSONObject(result.getContents());
-                    //set the values that are returned to the text views
-                    name = obj.getString("name");
-                    reg_no = obj.getString("reg_no");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    //If control comes here
-                    //It means that encoded format doesn't match
-                    //ie, User scanned a QR code which isn't in our DB
-                    Toast.makeText(this, "Wrong QR Code", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
