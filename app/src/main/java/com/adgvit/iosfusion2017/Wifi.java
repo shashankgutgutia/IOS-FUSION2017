5d25fa914c630ec79a3e589526436f495f6bc2dd
Allan Bastian <allan.batian9.ab@gmail.com>
2017-09-08 02:13:25 +0530

    Added firebase to attendance and wifi

--- app/src/main/java/com/adgvit/iosfusion2017/Wifi.java
+++ app/src/main/java/com/adgvit/iosfusion2017/Wifi.java
@@ -1,5 +1,6 @@
 package com.adgvit.iosfusion2017;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Wifi extends Fragment {
 
     public TextView WifiUserName, WifiPassWord, info, user, pass;
     public Button getDetails;
+    public SharedPreferences sp = getContext().getSharedPreferences("key", 0);
+    public String attendance;
 
     @Nullable
     @Override
@@ -30,12 +34,17 @@ public class Wifi extends Fragment {
                 /* check if attendance has been noted
                     if done, show all text views, else keep invisible and show error
                  */
+                attendance = sp.getString("attendance_status", "");
+                if(attendance == "Yes") {
                 WifiUserName.setVisibility(View.VISIBLE);
                 WifiPassWord.setVisibility(View.VISIBLE);
                 info.setVisibility(View.VISIBLE);
                 user.setVisibility(View.VISIBLE);
                 pass.setVisibility(View.VISIBLE);
-                getDetails.setVisibility(View.INVISIBLE);
+                getDetails.setVisibility(View.INVISIBLE); }
+                else {
+                    Toast.makeText(getContext(), "Attendance has not been marked", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
